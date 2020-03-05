c6eb6c0a79ad2c9e301dffee347494875bbd9e53
Bartego <matej.radosovsky@gmail.com>
2020-03-05 13:10:33 +0100

    Migration from Jupiter notebooks to VS studio and repo system on GitHub

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,6 +1,13 @@
 from random import random
 from random import seed
 
+from IPython.display import clear_output
+clear_output()
+
+layout_board = ["#",'7','8','9','4','5','6','1','2','3']
+game_board = ["#",'O','X','O','X','O','X','O','X','0']
+game_range = ['Q','1','2','3','4','5','6','7','8','9']
+
 
 def player_selection():
     a = random()
